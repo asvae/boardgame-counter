4b1efb882d676c1f39fc075a10cb3f29cdaf8c2e
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-08-26 14:19:08 +0400

    minor

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {createVuestic} from 'vuestic-ui'
 import 'vuestic-ui/css'
 import './assets/main.css'
 import 'virtual:uno.css'
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import MainPage from './components/main-page/MainPage.vue'
 import SettingsPage from './components/main-page/SettingsPage.vue'
 
@@ -12,7 +12,7 @@ import App from './App.vue'
 const app = createApp(App)
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {path: '/', component: MainPage},
     {path: '/settings', component: SettingsPage},
